2ca7b3e184af80e36bbe1050bbb77c44bf464f69
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-10 12:06:32 +0700

    add suspense

--- src/route.js
+++ src/route.js
@@ -1,18 +1,29 @@
 import { useRoutes } from 'react-router-dom';
 import { AuthTabs, ForgotPassword, ResetPassword } from './pages/auth';
-import Streams from '@pages/social/streams/Streams';
-import Social from '@pages/social/Socical';
-import Follower from '@pages/social/follower/Follower';
-import Following from '@pages/social/following/Following';
-import Photos from '@pages/social/photos/Photos';
-import Profile from '@pages/social/profile/Profile';
-import Chat from '@pages/social/chat/Chat';
-import People from '@pages/social/people/People';
-import Notifications from '@pages/social/notifications/Notifications';
+// import Streams from '@pages/social/streams/Streams';
+// import Social from '@pages/social/Socical';
+// import Follower from '@pages/social/follower/Follower';
+// import Following from '@pages/social/following/Following';
+// import Photos from '@pages/social/photos/Photos';
+// import Profile from '@pages/social/profile/Profile';
+// import Chat from '@pages/social/chat/Chat';
+// import People from '@pages/social/people/People';
+// import Notifications from '@pages/social/notifications/Notifications';
 import ProtectedRoute from '@pages/ProtectedRoute';
 import Error from '@pages/error/Error';
-import { Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import StreamsSkeleton from '@pages/social/streams/StreamsSkeleton';
+const Social = lazy(() => import('@pages/social/Socical'));
+const Chat = lazy(() => import('@pages/social/chat/Chat'));
+const Follower = lazy(() => import('@pages/social/follower/Follower'));
+const Following = lazy(() => import('@pages/social/following/Following'));
+const Notifications = lazy(() => import('@pages/social/notifications/Notifications'));
+const People = lazy(() => import('@pages/social/people/People'));
+const Photos = lazy(() => import('@pages/social/photos/Photos'));
+
+const Profile = lazy(() => import('@pages/social/profile/Profile'));
+const Streams = lazy(() => import('@pages/social/streams/Streams'));
+
 const AppRouter = () => {
     const elements = useRoutes([
         {
@@ -41,31 +52,31 @@ const AppRouter = () => {
                 },
                 {
                     path: "chat/messages",
-                    element: <Chat></Chat>
+                    element: <Suspense><Chat></Chat></Suspense>
                 },
                 {
                     path: "people",
-                    element: <People></People>
+                    element:  <Suspense><People></People></Suspense>
                 },
                 {
                     path: "followers",
-                    element: <Follower></Follower>
+                    element: <Suspense><Follower></Follower></Suspense>
                 },
                 {
                     path: "following",
-                    element: <Following></Following>
+                    element: <Suspense><Following></Following></Suspense>
                 },
                 {
                     path: "photos",
-                    element: <Photos></Photos>
+                    element: <Suspense><Photos></Photos></Suspense>
                 },
                 {
                     path: "notifications",
-                    element: <Notifications></Notifications>
+                    element: <Suspense><Notifications></Notifications></Suspense>
                 },
                 {
                     path: "profile/:username",
-                    element: <Profile></Profile>
+                    element: <Suspense><Profile></Profile></Suspense>
                 },
 
             ]
