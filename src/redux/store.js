58f14bf8453c636d9d3a7fb108d29517a6aa77bd
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-11 23:09:15 +0700

    add modal.reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '@redux/reducers/user/user.reducer';
 import suggestionsReducer from './reducers/suggestions/suggestions.reducer';
 import notificationsReducer from './reducers/notifications/notification.reducer';
+import modalReducer from './reducers/modal/modal.reducer';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     suggestions: suggestionsReducer,
-    notifications: notificationsReducer
+    notifications: notificationsReducer,
+    modal:modalReducer
   }
 });
