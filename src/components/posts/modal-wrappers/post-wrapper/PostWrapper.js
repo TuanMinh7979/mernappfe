e1816b7ee5bb2cadecb565c31edeb2d24d3f96d6
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-11 23:18:33 +0700

    add PostWrapper

---
+++ src/components/posts/modal-wrappers/post-wrapper/PostWrapper.js
@@ -0,0 +1,18 @@
+import "./PostWrapper.scss"
+import React from 'react'
+
+const PostWrapper = ({ children }) => {
+    return (
+        <div className="modal-wrapper" data-testid="post-modal">
+            {children[1]}
+            {children[2]}
+            {children[3]}
+            <div className="modal-bg"></div>
+        </div>
+    )
+}
+PostWrapper.propTypes = {
+    children: PropTypes.node.isRequired
+
+}
+export default PostWrapper
