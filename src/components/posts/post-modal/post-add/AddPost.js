ed6f3b7a9172b6eb5afa1a8c687ae70c31457081
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-12 08:48:00 +0700

    add  ModalBoxSelection

--- src/components/posts/post-modal/post-add/AddPost.js
+++ src/components/posts/post-modal/post-add/AddPost.js
@@ -4,9 +4,18 @@ import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import "./AddPost.scss"
 import ModalBoxContent from '../modal-box-content/ModalBoxContent'
+import { FaTimes } from 'react-icons/fa'
+import { bgColors } from '@services/utils/static.data'
+import ModalBoxSelection from '../modal-box-content/ModalBoxSelection'
+import Button from '@components/button/Button'
 const AddPost = () => {
     const { gifModalIsOpen } = useSelector(state => state.modal)
     const [loading] = useState(false)
+
+    const [postImage, setPostImage] = useState('');
+    const [allowedNumberOfCharacters] = useState('100/100');
+
+
     return (
         <PostWrapper>
             <div></div>
@@ -25,6 +34,93 @@ const AddPost = () => {
                     </div>
                     <hr />
                     <ModalBoxContent></ModalBoxContent>
+
+                    {!postImage && (
+                        <>
+                            <div
+                                className="modal-box-form"
+                                data-testid="modal-box-form"
+
+                            >
+                                <div className="main" >
+                                    <div className="flex-row">
+                                        <div
+                                            data-testid="editable"
+                                            id="editable"
+                                            name="post"
+
+                                            className='editable'
+                                            contentEditable={true}
+
+                                            data-placeholder="What's on your mind?..."
+                                        ></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    )}
+
+                    {postImage && (
+                        <>
+                            <div className="modal-box-image-form">
+                                <div
+                                    data-testid="post-editable"
+                                    name="post"
+                                    id="editable"
+
+                                    className="post-input flex-item"
+                                    contentEditable={true}
+                                    data-placeholder="What's on your mind?..."
+                                ></div>
+                                <div className="image-display">
+                                    <div
+                                        className="image-delete-btn"
+                                        data-testid="image-delete-btn"
+
+                                    >
+                                        <FaTimes />
+                                    </div>
+                                    <img data-testid="post-image" className="post-image" src={`${postImage}`} alt="" />
+
+                                </div>
+                            </div>
+                        </>
+                    )}
+                    <div className="modal-box-bg-colors">
+                        <ul>
+
+                            {bgColors.map((color, index) =>
+
+                                <li
+                                    data-testid="bg-colors"
+                                    key={index}
+                                    className={`${color === "#ffffff" ? 'whiteColorBorder' : ''}`}
+                                    style={{ backgroundColor: `${color}` }}
+                                >
+
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                    <span className='char_count' data-testid="allowed-number">
+                        {allowedNumberOfCharacters}
+                    </span>
+
+                    <ModalBoxSelection></ModalBoxSelection>
+
+                    <div className="modal-box-button" data-testid="post-button">
+
+                        <Button label="Create Post" className="post-button"
+                            disable={true}>
+
+
+                        </Button>
+                    </div>
+
+
+
+
+
                 </div>
 
             }
