5c41abb374ea781fd2e6bf7623a7216be13b8f1b
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-15 22:27:58 +0700

    ad ReactionAndCommentDisplay

--- src/components/posts/post-comment-section/PostCommentSection.js
+++ src/components/posts/post-comment-section/PostCommentSection.js
@@ -1,10 +1,11 @@
 import CommentArea from '../comment-area/CommentArea';
 import PropTypes from 'prop-types';
+import ReactionsAndCommentDisplay from '../reactions/reactions-comments-display/ReactionsAndCommentsDisplay';
 
 const PostCommentSection = ({ post }) => {
     return (
         <div data-testid="comment-section">
-
+            <ReactionsAndCommentDisplay post={post} />
             <CommentArea post={post} />
         </div>
     );
