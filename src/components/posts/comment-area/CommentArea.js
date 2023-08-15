d7cd4af20a99439f0aa2dfbaa2a731ca231d488c
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-15 22:16:34 +0700

    add reactions layout

--- src/components/posts/comment-area/CommentArea.js
+++ src/components/posts/comment-area/CommentArea.js
@@ -3,8 +3,18 @@ import { FaRegCommentAlt } from 'react-icons/fa';
 import "./CommentArea.scss"
 import PropTypes from 'prop-types';
 import like from "@assets/reactions/like.png"
-
+import Reactions from '../reactions/Reactions';
+import { Utils } from '@services/utils/utils.service';
+import { useDispatch } from 'react-redux';
 const CommentArea = ({ post }) => {
+    const dispatch = useDispatch()
+    const onReactionClick = async (reaction) => {
+        try {
+            console.log("ADD REACTYion");
+        } catch (error) {
+            Utils.updToastsNewEle(error?.response?.data?.message, 'error', dispatch);
+        }
+    };
     return (
         <div className="comment-area" data-testid="comment-area">
             <div className="like-icon reactions">
@@ -21,7 +31,7 @@ const CommentArea = ({ post }) => {
                     </div>
                 </div>
                 <div className="reactions-container app-reactions">
-                    Reactions
+                    <Reactions handleClick={onReactionClick}></Reactions>
                 </div>
             </div>
             <div className="comment-block" >
---
+++ src/components/posts/reactions/Reactions.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Reactions.scss"
+import PropTypes from 'prop-types';
+import { reactionsMap } from '@services/utils/static.data';
+const Reactions = ({ handleClick, showLabel = true }) => {
+    const reactionList = ['like', 'love', 'wow', 'happy', 'sad', 'angry'];
+    return (
+        <div className="reactions" data-testid="reactions">
+            <ul>
+                {reactionList.map((reaction, index) => (
+                    <li key={index} onClick={() => handleClick(reaction)} data-testid="reaction">
+                        {showLabel && <label>{reaction}</label>}
+                        <img src={reactionsMap[reaction]} alt="" />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+Reactions.propTypes = {
+    handleClick: PropTypes.func,
+    showLabel: PropTypes.bool
+};
+export default Reactions
