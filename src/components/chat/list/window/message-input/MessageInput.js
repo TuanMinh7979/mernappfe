e2109d93b2ecae0232c64b25c17256827b087db2
TuanMinh7979 <minhtuanto179@gmail.com>
2023-08-23 11:56:18 +0700

    add gif container

--- src/components/chat/list/window/message-input/MessageInput.js
+++ src/components/chat/list/window/message-input/MessageInput.js
@@ -9,83 +9,109 @@ import feeling from '@assets/images/feeling.png';
 import './MessageInput.scss';
 import { FaPaperPlane } from 'react-icons/fa';
 import loadable from '@loadable/component';
-
+import { useState } from 'react';
+import GiphyContainer from '@components/chat/giphy-container/GiphyContainer';
 const EmojiPickerComponent = loadable(() => import('./EmojiPicker'), {
     fallback: <p id="loading">Loading...</p>
-  });
-  
+});
+
 const MessageInput = ({ setChatMessage }) => {
-//   let [message, setMessage] = useState('');
-//   const [showEmojiContainer, setShowEmojiContainer] = useState(false);
-//   const [showGifContainer, setShowGifContainer] = useState(false);
-//   const [showImagePreview, setShowImagePreview] = useState(false);
-//   const [file, setFile] = useState();
-//   const [base64File, setBase64File] = useState('');
-//   const [hasFocus, setHasFocus] = useState(false);
-//   const fileInputRef = useRef();
-//   const messageInputRef = useRef();
+    //   let [message, setMessage] = useState('');
+    const [showEmojiContainer, setShowEmojiContainer] = useState(false);
+    const [showGifContainer, setShowGifContainer] = useState(false);
+    //   const [showImagePreview, setShowImagePreview] = useState(false);
+    //   const [file, setFile] = useState();
+    //   const [base64File, setBase64File] = useState('');
+    //   const [hasFocus, setHasFocus] = useState(false);
+    //   const fileInputRef = useRef();
+    //   const messageInputRef = useRef();
 
+    const handleGiphyClick=()=>{
+        
+    }
 
+    return (
+        <>
 
-  return (
-    <>
+            {showEmojiContainer && (
+                <EmojiPickerComponent
+                    onEmojiClick={(event, eventObject) => {
+                        console.log("set mesage");
+                        // setMessage((text) => (text += ` ${eventObject.emoji}`));
+                    }}
+                    pickerStyle={{ width: '352px', height: '447px' }}
+                />
+            )}
 
-   
-      <div className="chat-inputarea" data-testid="chat-inputarea">
+            {showGifContainer && <GiphyContainer handleGiphyClick={handleGiphyClick} />}
 
-        <form >
-          <ul className="chat-list" 
-          >
-            <li
-              className="chat-list-item"
-              
-            >
-              <Input
-              
-                id="image"
-                name="image"
-                type="file"
-                className="file-input"
-                placeholder="Select file"
-                
-              />
-              <img src={photo} alt="" />
-            </li>
-            <li
-              className="chat-list-item"
-           
-            >
-              <img src={gif} alt="" />
-            </li>
-            <li
-              className="chat-list-item"
-             
-            >
-              <img src={feeling} alt="" />
-            </li>
-          </ul>
-          <Input
-          
-            id="message"
-            name="message"
-            type="text"
-            
-            className="chat-input"
-            labelText=""
-            placeholder="Enter your message..."
-         
-          />
-           <Button label={<FaPaperPlane />} className="paper"  />
+            <div className="chat-inputarea" data-testid="chat-inputarea">
 
-        </form>
-       
-      </div>
-    </>
-  );
+                <form >
+                    <ul className="chat-list"
+                    >
+                        <li
+                            className="chat-list-item"
+                            onClick={() => {
+
+                                setShowEmojiContainer(false);
+
+                            }}
+                        >
+                            <Input
+
+                                id="image"
+                                name="image"
+                                type="file"
+                                className="file-input"
+                                placeholder="Select file"
+
+                            />
+                            <img src={photo} alt="" />
+                        </li>
+                        {/* Gif */}
+                        <li
+                            className="chat-list-item"
+                            onClick={() => {
+                                setShowGifContainer(!showGifContainer)
+                                setShowEmojiContainer(false);
+                            }}
+                        >
+                            <img src={gif} alt="" />
+                        </li>
+                        {/* Emoji */}
+                        <li
+                            className="chat-list-item"
+                            onClick={() => {
+                                setShowEmojiContainer(!showEmojiContainer);
+                            }}
+
+                        >
+                            <img src={feeling} alt="" />
+                        </li>
+                    </ul>
+                    <Input
+
+                        id="message"
+                        name="message"
+                        type="text"
+
+                        className="chat-input"
+                        labelText=""
+                        placeholder="Enter your message..."
+
+                    />
+                    <Button label={<FaPaperPlane />} className="paper" />
+
+                </form>
+
+            </div>
+        </>
+    );
 };
 
 MessageInput.propTypes = {
-  setChatMessage: PropTypes.func
+    setChatMessage: PropTypes.func
 };
 
 export default MessageInput;
