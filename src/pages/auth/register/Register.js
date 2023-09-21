6bb573c8debe7ed0f1b77577590f2fbfe2cf9c67
TuanMinh7979 <minhtuanto179@gmail.com>
2023-09-21 22:20:16 +0700

    fix register

--- src/pages/auth/register/Register.js
+++ src/pages/auth/register/Register.js
@@ -8,66 +8,57 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import useSessionStorage from '@hooks/useSessionStorage';
 const Register = () => {
-    const [username, setUserName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+
+
+
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const onInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+
     const [loading, setLoading] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
-    const [alertType, setAlertType] = useState('')
-    const [hasError, setHasError] = useState(false)
-    const [user, setUser] = useState();
-    const navigate= useNavigate()
-
-    const dispatch= useDispatch();
-    const [pageReload]= useSessionStorage('pageReload', 'set')
-
     const registerUser = async (event) => {
-        setLoading(true)
+
         event.preventDefault();
         try {
+            setErrorMessage("")
+            setLoading(true)
             const avatarColor = Utils.avatarColor()
-            const avatarImage = Utils.generateAvatar(username.charAt(0).toUpperCase(), avatarColor)
+            const avatarImage = Utils.generateAvatar(formData.username.charAt(0).toUpperCase(), avatarColor)
             const rs = await authService.signUp({
-                username,
-                email,
-                password,
+                username: formData.username,
+                email: formData.email,
+                password: formData.password,
                 avatarColor,
                 avatarImage
             })
-            console.log(rs);
-
-            // * set logged in local storage
-            // * set usename in local storage
-            // * dispatch user to redux
             setLoading(false)
-            setUser(rs.data.user)
-            setHasError(false);
-            setAlertType('alert-success')
-            Utils.dispatchUser(rs, pageReload, dispatch,setUser)
-
         } catch (error) {
             setLoading(false);
-            setHasError(true);
-            setAlertType('alert-error')
             setErrorMessage(error?.response?.data.message)
         }
 
     }
 
-    useEffect(() => {
-        if (loading && !user) return;
-        if (user) {
-            navigate('/app/social/streams')
-            setLoading(false)
-        }
-    }, [loading, user])
+// TODO: create global alert for success register
     return (
         <div className="auth-inner">
             <div className={`alerts`} role="alert">
-                {hasError && errorMessage &&
-                    <div className={`alerts ${alertType}`} role='alert'>
+                {errorMessage &&
+                    <div className={`alerts alert-error`} role='alert'>
                         {errorMessage}
-                    </div>
+                    </div> 
                 }
             </div>
             <form className="auth-form" onSubmit={registerUser}>
@@ -76,37 +67,37 @@ const Register = () => {
                         id="username"
                         name="username"
                         type="text"
-                        value={username}
+                        value={formData.username}
                         labelText="Username"
                         placeholder="Enter Username"
-                        handleChange={(event) => setUserName(event.target.value)}
-                        style={{ border: `${hasError ? '1px solid #fa9b8a' : ''}` }}
+                        handleChange={onInputChange}
+                        style={{ border: `${errorMessage ? '1px solid #fa9b8a' : ''}` }}
                     />
                     <Input
-                        value={email}
+                        value={formData.email}
                         id="email"
                         name="email"
                         type="text"
                         labelText="Email"
                         placeholder="Enter Email"
-                        handleChange={(event) => setEmail(event.target.value)}
+                        handleChange={onInputChange}
 
                     />
                     <Input
-                        value={password}
+                        value={formData.password}
                         id="password"
                         name="password"
                         type="password"
                         labelText="Password"
                         placeholder="Enter Password"
-                        handleChange={(event) => setPassword(event.target.value)}
+                        handleChange={onInputChange}
 
                     />
                 </div>
                 <Button
                     label={`${loading ? 'SIGNUP IN PROGRESS...' : 'SIGNUP'}`}
                     className="auth-button button"
-                    disabled={!username || !email || !password}
+                    disabled={!formData.username || !formData.email || !formData.password}
                 />
             </form>
         </div>
